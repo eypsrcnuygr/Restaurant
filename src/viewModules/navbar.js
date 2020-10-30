e10db9ba517c9d0fc3851ad619592585be564446
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-30 12:38:30 +0300

    Change the order of the nav and the div

--- src/viewModules/navbar.js
+++ src/viewModules/navbar.js
@@ -1,11 +1,12 @@
 const navCreator = () => {
   let body = document.querySelector('body');
-  let navbarContainer = document.getElementById('content');
+  let navbarContainer = document.createElement('header')
   let unorderedList = document.createElement('ul');
   let list = document.createElement('li');
   let anchor = document.createElement('a');
   let secondList = document.createElement('li');
   let secondAnchor = document.createElement('a');
+  let content = document.getElementById('content')
   anchor.textContent = 'Menu';
   secondAnchor.textContent = 'Contact';
   list.appendChild(anchor);
@@ -14,12 +15,15 @@ const navCreator = () => {
   unorderedList.appendChild(secondList);
   navbarContainer.appendChild(unorderedList);
   body.appendChild(navbarContainer);
+
+  body.insertBefore(navbarContainer, content)
   unorderedList.setAttribute('class', 'nav nav-tabs list-unstyled');
   list.setAttribute('class', 'nav-item list-inline-item');
   secondList.setAttribute('class', 'nav-item list-inline-item');
-  anchor.setAttribute('class', 'nav-link');
-  secondAnchor.setAttribute('class', 'nav-link')
-  unorderedList.setAttribute('class', 'd-flex justify-content-center')
+  anchor.setAttribute('class', 'nav-link btn text-light');
+  secondAnchor.setAttribute('class', 'nav-link btn text-light')
+  unorderedList.setAttribute('class', 'd-flex justify-content-center mb-0')
+  navbarContainer.setAttribute('class', 'bg-secondary')
 }
 
 export {navCreator}
