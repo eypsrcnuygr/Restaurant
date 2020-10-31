2b3f9292e425300f7472e8169cedbbc83b18c48d
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-31 20:10:26 +0300

    Add working menu and home page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import {navCreator} from './viewModules/navbar'
+import {menuCreator} from './viewModules/Menu'
 
-window.addEventListener('load', () => {
+
   navCreator()
-});
+
+  let clearPage = () => {
+    let section = document.querySelector('.modifySection')
+    section.remove()
+  }
+
+
+  firstAnchor.addEventListener('click', () => {
+    document.querySelector('#content').setAttribute('class', 'withBackground');
+    if (document.querySelector('.modifySection')) {
+      clearPage()
+    }
+    
+  })
+
+  secondAnchor.addEventListener('click', () => {
+    document.querySelector('#content').setAttribute('class', 'withouthBackground');
+    if (!document.querySelector('.modifySection')) {
+      menuCreator(); 
+    } 
+  })
+
+
--- src/viewModules/navbar.js
+++ src/viewModules/navbar.js
@@ -6,13 +6,18 @@ const navCreator = () => {
   let anchor = document.createElement('a');
   let secondList = document.createElement('li');
   let secondAnchor = document.createElement('a');
+  let thirdList = document.createElement('li');
+  let thirdAnchor = document.createElement('a');
   let content = document.getElementById('content')
-  anchor.textContent = 'Menu';
-  secondAnchor.textContent = 'Contact';
+  anchor.textContent = 'Home';
+  secondAnchor.textContent = 'Menu';
+  thirdAnchor.textContent = 'Contact'
   list.appendChild(anchor);
   secondList.appendChild(secondAnchor);
+  thirdList.appendChild(thirdAnchor);
   unorderedList.appendChild(list);
   unorderedList.appendChild(secondList);
+  unorderedList.appendChild(thirdList);
   navbarContainer.appendChild(unorderedList);
   body.appendChild(navbarContainer);
 
@@ -20,10 +25,19 @@ const navCreator = () => {
   unorderedList.setAttribute('class', 'nav nav-tabs list-unstyled');
   list.setAttribute('class', 'nav-item list-inline-item');
   secondList.setAttribute('class', 'nav-item list-inline-item');
+  thirdList.setAttribute('class', 'nav-item list-inline-item');
   anchor.setAttribute('class', 'nav-link btn text-light');
+  anchor.setAttribute('id', 'firstAnchor');
+  secondAnchor.setAttribute('id', 'secondAnchor');
   secondAnchor.setAttribute('class', 'nav-link btn text-light')
+  thirdAnchor.setAttribute('class', 'nav-link btn text-light')
   unorderedList.setAttribute('class', 'd-flex justify-content-center mb-0')
   navbarContainer.setAttribute('class', 'bg-secondary')
+  return {
+    firstAnchor,
+    secondAnchor,
+    thirdAnchor
+  }
 }
 
 export {navCreator}
